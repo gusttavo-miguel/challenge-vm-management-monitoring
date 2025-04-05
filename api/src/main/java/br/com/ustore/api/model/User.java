5adc89e16b5d7b9fcb88820494bcd5de0b8cdea6
Gustavo Miguel <gusttavo.miguel.silva@gmail.com>
2025-04-05 16:31:29 -0300

    validação do campo email do userDTO

--- api/src/main/java/br/com/ustore/api/model/User.java
+++ api/src/main/java/br/com/ustore/api/model/User.java
@@ -1,14 +1,16 @@
 package br.com.ustore.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @Entity
 @Table(name = "users")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -18,6 +20,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
     @Column(nullable = false, unique = true)
     private String email;
 
